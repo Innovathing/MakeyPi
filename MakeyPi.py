498b8d1c52930d1f3d326819ccae7c1148a503c7
thetlk <jeremieboutoille@gmail.com>
2015-04-05 17:11:52 +0200

    Python is better than java :)

---
+++ MakeyPi.py
@@ -0,0 +1,5 @@
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Take picture when a key is pressed :o')
+args = parser.parse_args()
--- README.md
+++
@@ -1,2 +0,0 @@
-# MakeyPi
-A connected Bird table to take a picture every time it lands
